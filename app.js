114ac338c7ebb8f294169c926c95da0140075d75
Avani Sindhav <avani@simformsolutions.com>
2022-07-26 15:56:10 +0530

    typescript types in action

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Your code Goes Here !");
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log("".concat(phrase, " ").concat(result));
+    }
+    return n1 + n2;
+}
+var number1 = 56;
+var number2 = 45.7;
+var printResult = true;
+var printPhrase = "Result is: ";
+var result = add(number1, number2, printResult, printPhrase);
+// console.log(result);
