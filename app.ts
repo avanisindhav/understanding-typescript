2bc4c795142734099779935f5e9ecfad6dbe6d4b
Avani Sindhav <avani@simformsolutions.com>
2022-07-26 17:05:08 +0530

    test: nested object types
    Learning-TypeSript Udemy Course - Section 2
    
    Closes video 17

--- app.ts
+++ app.ts
@@ -1,9 +1,26 @@
 const person: {
   name: string;
   age: number;
+  details: {
+    hobby: string;
+  };
 } = {
   name: "Avani",
   age: 23,
+  details: {
+    hobby: "singing",
+  },
+};
+
+//  inference the object type by typescript
+const product = {
+  id: "abc1",
+  price: 12.89,
+  tags: ["great-offers", "hot-and-new"],
+  details: {
+    title: "Red Carpet",
+    description: "This is Good Product",
+  },
 };
 
 console.log(person.name);
