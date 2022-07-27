2c6d76df71aea5552568142865e7bd73fc354da4
Avani Sindhav <avani@simformsolutions.com>
2022-07-27 17:06:19 +0530

    test: Types Alias / Custom Types
    Learning-TypeSript Udemy Course - Section 2
    
    Closes video 24

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+//we can use custom types like above to avoid confusion and s
 function combine(input1, input2, resultConversion) {
     var result;
     if ((typeof input1 === "number" && typeof input2 === "number") ||
-        // resultConversion === "as-numbr" // if i write this tyypo can be catch by typescript
         resultConversion === "as-number") {
         result = +input1 + +input2;
     }
@@ -14,8 +14,3 @@ var combineAgesAsNumber = combine(12, 23, "as-number");
 console.log(combineAgesAsNumber);
 var combineAgesAsText = combine(12, 23, "as-text");
 console.log(combineAgesAsText);
-// const combineName = combine("Aani", "sindhav", "as-tex"); //this is also throw error so we catch issue using typescript
-// we can use enum too but for only two value we can use literal types
-// literal is specific value
-var number = 2.5;
-// number = 6; //this throw error in typescript so ;like const number = 2.5; like this it will work as lireal type
--- app.ts
+++ app.ts
@@ -1,13 +1,17 @@
+type Combinable = number | string;
+type resultConversionType = "as-number" | "as-text";
+
+//we can use custom types like above to avoid confusion and s
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: resultConversionType
 ) {
   let result;
 
   if (
     (typeof input1 === "number" && typeof input2 === "number") ||
-    // resultConversion === "as-numbr" // if i write this tyypo can be catch by typescript
     resultConversion === "as-number"
   ) {
     result = +input1 + +input2;
@@ -23,12 +27,3 @@ console.log(combineAgesAsNumber);
 
 const combineAgesAsText = combine(12, 23, "as-text");
 console.log(combineAgesAsText);
-
-// const combineName = combine("Aani", "sindhav", "as-tex"); //this is also throw error so we catch issue using typescript
-
-// we can use enum too but for only two value we can use literal types
-// literal is specific value
-
-let number: 2.5 = 2.5;
-
-// number = 6; //this throw error in typescript so ;like const number = 2.5; like this it will work as lireal type
