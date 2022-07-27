82b80d04970cec07701750c75f28d040f68d80bf
Avani Sindhav <avani@simformsolutions.com>
2022-07-27 18:55:19 +0530

    test: 27. Functions as Types
    Learning-TypeSript Udemy Course - Section 2
    
    Closes video 27

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-// function add(n1: number, n2: number): number {
-//   return n1 + n2;
-// }
-// //  you can specify return type after parameter :type as seen in above function
 function add(n1, n2) {
     return n1 + n2;
 }
-//  typescript can infere the return type itself too
-// function printResult(result: number) {
-//   console.log("Result is :", result);
-// }
-//if function does not return anything then return type of funtion will be void
-// even if you try to console console.log(printResult(23)) will return undefined
-// if you wanna pass the return type as undefined then need to pass return; statement in function luike below
-// function printResult(result: number): undefined {
-//   console.log("Result is :", result);
-//   return;
-// }
-// otherwise we can use void withoiut return and typescript can also infer thi stype
 function printResult(result) {
-    console.log("Result is :", result);
+    console.log(result);
 }
-console.log(printResult(add(2, 3)));
+var combineValues;
+combineValues = add;
+// combineValues = printResult; //return TYpeScript ERROR !!!
+// let combineValues: Function;
+// combineValues = add;
+// combineValues = 2; // not allowed as intialise since define type Function
+console.log(combineValues(2, 3));
