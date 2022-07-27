68acfd5999fbb94abcc857445757c106c010815f
Avani Sindhav <avani@simformsolutions.com>
2022-07-27 11:28:43 +0530

    test: Working with Enums
    Learning-TypeSript Udemy Course - Section 2
    
    Closes video 20

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-// const person = {
-//   name: "Avani",
-//   age: 22,
-//   hobbies: ["Sports", "Cooking", "Reading"],
-//   role: [2284, "Software Engineer"],
-// };
-//now we want Above Object role as tuples
-//  tuples are not core datatype and not supported by javascript its typescript database
-//  tuples are fixed length and fixed typed array
-//  this also not done by typescript inference so need to define manually
-//  like in above example we want role to be tuple that in a way that role must have 2 length
-//  first will be employee id and second will be the designation string
+//enum
+var Role;
+(function (Role) {
+    Role[Role["USER"] = 0] = "USER";
+    Role[Role["ADMIN"] = 1] = "ADMIN";
+    Role[Role["SUPERADMIN"] = 2] = "SUPERADMIN";
+    Role["TASKER"] = "Tasker not";
+})(Role || (Role = {}));
 var person = {
     name: "Avani",
-    age: 22,
-    hobbies: ["Sports", "Cooking", "Reading"],
-    role: [2284, "Software Engineer"]
+    role: Role.USER
 };
-// now if we try to replace employee id with string this will yell
-// person.role[0] = "strings"
-// same if we try to change length using or above any case error will be thrown
-// person.role = [2284];
-// person.role = [];
-// person.role = [2284, "Software Engineer", "Simform Solution"];
-// person.role = ["string", "number"];
-// push is the exception that typescript is not able to detect
-person.role.push("Simform Solution");
-console.log(person);
+person.role = Role.TASKER;
+console.log(person.role);
--- app.ts
+++ app.ts
@@ -1,39 +1,20 @@
-// const person = {
-//   name: "Avani",
-//   age: 22,
-//   hobbies: ["Sports", "Cooking", "Reading"],
-//   role: [2284, "Software Engineer"],
-// };
+//enum
 
-//now we want Above Object role as tuples
-//  tuples are not core datatype and not supported by javascript its typescript database
-//  tuples are fixed length and fixed typed array
-//  this also not done by typescript inference so need to define manually
-//  like in above example we want role to be tuple that in a way that role must have 2 length
-//  first will be employee id and second will be the designation string
+enum Role {
+  USER = 7,
+  ADMIN = 9,
+  SUPERADMIN,
+  TASKER = "Tasker not",
+}
 
 const person: {
   name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; //this is the way tuples are identify by the typescript
+  role: Role;
 } = {
   name: "Avani",
-  age: 22,
-  hobbies: ["Sports", "Cooking", "Reading"],
-  role: [2284, "Software Engineer"],
+  role: Role.USER,
 };
 
-// now if we try to replace employee id with string this will yell
-// person.role[0] = "strings"
+person.role = Role.TASKER;
 
-// same if we try to change length using or above any case error will be thrown
-// person.role = [2284];
-// person.role = [];
-// person.role = [2284, "Software Engineer", "Simform Solution"];
-// person.role = ["string", "number"];
-
-// push is the exception that typescript is not able to detect
-person.role.push("Simform Solution");
-
-console.log(person);
+console.log(person.role);
